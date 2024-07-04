02acba4807868bd900846b86fa03e15196531def
yjwfss <144475604+yjwfss@users.noreply.github.com>
2024-07-04 20:58:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.shilin.asia": dockerHub,
+  "quay.shilin.asia": "https://quay.io",
+  "gcr.shilin.asia": "https://gcr.io",
+  "k8s-gcr.shilin.asia": "https://k8s.gcr.io",
+  "k8s.shilin.asia": "https://registry.k8s.io",
+  "ghcr.shilin.asia": "https://ghcr.io",
+  "cloudsmith.shilin.asia": "https://docker.cloudsmith.io",
+  "ecr.shilin.asia": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
